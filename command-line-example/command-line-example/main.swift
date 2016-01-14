073c89ac4b310afe7a7185d0db8905b7014927f0
Liam Byrne <student@MacBook-Pro-Liam-Byrne.local>
2016-01-14 18:05:04 -0500

    Updated command line, and started the user menu

--- command-line-example/command-line-example/main.swift
+++ command-line-example/command-line-example/main.swift
@@ -2,31 +2,39 @@
 //  main.swift
 //  basic-command-line
 //
-//  Created by Russell Gordon on 2015-12-16.
+//  Created by Liam Byrne on 2016-12-16.
 //  Copyright © 2015 Royal St. George's College. All rights reserved.
 //
 
 import Foundation
 let answer:Int = 0 // The variable to hold the correct answer depending on the random number
 let choice:Int = 0 // The variable to hold the user input
-var level = Int(readLine(stripNewline: true)) // The variable to hold the level the user selects
 var difficulty:UInt32 = 0
 
-let random1 = Int(arc4random_uniform(difficulty))
-let random2 = Int(arc4random_uniform(difficulty))
+print ("Welcome to Liam's Math Flash Cards!")
+print ("Please select a operator!")
+print ("1. +")
+print ("2. -")
+print ("3. *")
+print ("4. /")
 
 // Level system (Easy, Medium, Hard)
-
+let level = Int(readLine(stripNewline: true)!)! // The variable to hold the level the user selects
 if level == 1 {
     difficulty = 10
 } else if level == 2 {
-    difficulty == 50
+    difficulty = 50
 } else if level == 3 {
-    difficulty == 100
+    difficulty = 100
 } else {
     print("Error! That is not a valid choice")
 }
 
+let random1 = Int(arc4random_uniform(difficulty + 5))
+let random2 = Int(arc4random_uniform(difficulty + 5))
+
+print("\(random1) + \(random2)")
+2
 if choice == 1 { // if user selects addition, do this
     if answer == (random1+random2) {
         print ("correct!")
